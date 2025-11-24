1a8de480af76f8da19427a178756e3a4a1c2c6a7
Eugene Cho <66149270+Echo108471@users.noreply.github.com>
2025-11-24 02:32:13 -0800

    Update project_section.tsx

--- src/app/components/project_section.tsx
+++ src/app/components/project_section.tsx
@@ -7,8 +7,15 @@ const Projects: React.FC = () => {
       date: "Oct. 2024 - Current",
       name: "Cattlelog",
       description: "Course Recommendation Tool built for UC Davis Students",
-      techStack: ["React","Typescript","TailwindCSS", "FastAPI", "PostgreSQL", "Redis", "Neptune"],
+      techStack: ["React", "Typescript", "TailwindCSS", "FastAPI", "PostgreSQL", "Redis", "Neptune"],
       link: "https://daviscattlelog.com"
+    },
+    {
+      date: "Jun. 2025",
+      name: "Korean Quiz",
+      description: "Full-stack Korean vocabulary learning platform",
+      techStack: ["React", "Javascript", "Tailwind", "Express", "SQLite3"],
+      link: "https://koreanquiz.onrender.com/",
     },
     {
       date: "Jan. 2025",
@@ -21,7 +28,7 @@ const Projects: React.FC = () => {
       date: "Dec. 2024",
       name: "Resilient Files",
       description: "Decentralized file storage system using IPFS and ResilientDB",
-      techStack: ["React","Typescript", "IPFS", "ResilientDB", "Python", "FastAPI"],
+      techStack: ["React", "Typescript", "IPFS", "ResilientDB", "Python", "FastAPI"],
       link: "https://github.com/ResilientApp/decentralized-sys",
     },
     {
@@ -33,13 +40,6 @@ const Projects: React.FC = () => {
     },
     {
       date: "Sep. 2024",
-      name: "Korean Quiz",
-      description: "Full-stack Korean vocabulary learning platform",
-      techStack: ["React", "Javascript", "Tailwind", "Express", "SQLite3"],
-      link: "https://koreanquiz.onrender.com/",
-    },
-    {
-      date: "Sep. 2024",
       name: "PNA Tool",
       description: "Web tool designed to give information about PNA oligos for optimal design",
       techStack: ["PHP", "HTML", "CSS", "Python", "JSON"],
@@ -47,25 +47,25 @@ const Projects: React.FC = () => {
     },
   ];
 
-return (
-  <section className="bg-white-100 py-10">
-    <div className="container mx-auto px-4">
-      <h2 className="text-2xl font-semibold mb-6">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            date={project.date}
-            name={project.name}
-            description={project.description}
-            techStack={project.techStack}
-            link={project.link}
-          />
-        ))}
+  return (
+    <section className="bg-white-100 py-10">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-semibold mb-6">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              date={project.date}
+              name={project.name}
+              description={project.description}
+              techStack={project.techStack}
+              link={project.link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
 }
 
 
